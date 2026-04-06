23914a4493647d1cbfdc3f42f678cc7b926d52a5
Jein <djeinabou.bodie@gmail.com>
2026-04-06 00:54:14 -0700

    change to host as static site

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const gameData = require('./data/game.json');
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: `/staticGames/${gameData['game-id']}/`,
+    base: env.PARENT_REPO_BUILD ? `/staticGames/${gameData['game-id']}/` : '/',
     define: {
       __OPENAI_API_KEY__: JSON.stringify(env.VITE_OPENAI_API_KEY),
     },
